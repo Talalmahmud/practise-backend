eb2c0dfc2e02fae2a8a9d3cbab2b10b6f89b72cd
Talal Mahmud <mahmudtalal2@gmail.com>
2024-06-24 11:13:33 +0600

    added user add request

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { getUser } from "../controllers/user.js";
+import { addUser, getUser } from "../controllers/user.js";
 
 const router = express.Router();
 
-router.get("/user", getUser);
+router.get("/user", getUser).post("/user", addUser);
 
 export default router;
