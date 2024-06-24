d04956c4922e34e423597344c99aa50ef64aac0b
Talal Mahmud <mahmudtalal2@gmail.com>
2024-06-24 10:21:42 +0600

    changes

--- api/index.js
+++ api/index.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
 });
 
 app.listen(3001, () => {
-  console.log(`Server is running:http://localhost:${3001}`);
+  console.log(`Server is running on:http://localhost:${3001}`);
 });
 export default app;
