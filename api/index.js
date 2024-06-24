5ed49e9a55948c8ef1290e9d6a225132cd0af3b9
Talal Mahmud <mahmudtalal2@gmail.com>
2024-06-24 13:16:46 +0600

    changes

--- api/index.js
+++ api/index.js
@@ -1,10 +1,15 @@
 import express from "express";
 import userRoute from "./routes/user.js";
+import cors from "cors";
 const app = express();
+
+app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1", userRoute);
 app.get("*", (req, res, err) => {
-  res.status(200).json({ message: "Hello" });
+  res.status(200).json({ message: err });
 });
 
 app.listen(3001, () => {
